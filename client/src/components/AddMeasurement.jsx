5429334ddf29a31776899dd485ca5c01f3e4f15b
Brandon Hsu <brandon.j.hsu@gmail.com>
2022-12-21 20:21:57 -0800

    able to toggle medicine between the recommended and taken lists

--- client/src/components/AddMeasurement.jsx
+++ client/src/components/AddMeasurement.jsx
@@ -9,17 +9,20 @@ function AddMeasurement  ()  {
   const fullMedList = [
     {
       name: 'Motrin',
-      dosage: '5 mg'
+      dosage: '5 mg',
+      id: 0,
     },
     {
       name: 'Advil',
-      dosage: '10 mg'
+      dosage: '10 mg',
+      id: 1,
     },
     {
       name: 'Tylenol',
-      dosage: '15 mg'
+      dosage: '15 mg',
+      id: 2,
     },
-  ]
+  ];
 
   const [diastolic, setDiastolic] = useState();
   const [systolic, setSystolic] = useState();
@@ -52,42 +55,52 @@ function AddMeasurement  ()  {
     }
   }
 
-  const handleMed = (e) => {
-    console.log(e.currentTarget.value);
-    console.log (recMedList[e.currentTarget.value])
+  const addMed = (e) => {
+    let index = e.currentTarget.value;
 
     let temp = [...recMedList];
-    temp.splice(e.currentTarget.value, 1);
+    temp.splice(index, 1);
     setRecMedList(temp);
 
-
-    let medicine = recMedList[e.currentTarget.value];
+    let medicine = recMedList[index];
     let newMedList = [...takenMedList];
     newMedList.push(medicine);
+    newMedList.sort(function (a,b) {
+      return a.id - b.id;
+    });
+    setTakenMedList(newMedList);
+  }
 
-    setTakenMedList(newMedList)
-    // console.log (recMedList, takenMedList)
+  const removeMed = (e) => {
+    let index = e.currentTarget.value;
 
+    let temp = [...takenMedList];
+    temp.splice(index, 1);
+    setTakenMedList(temp);
+
+    let medicine = takenMedList[index];
+    let newMedList = [...recMedList];
+    newMedList.push(medicine);
+    newMedList.sort(function (a,b) {
+      return a.id - b.id;
+    });
+    setRecMedList(newMedList);
   }
 
   return (
     <div>
       <Stack spacing= {5} direction= 'row'>
-
         <TextField
           required
           id="outlined-basic"
           label="Diastolic"
           className= 'bloodpressure-field'
-
           onChange= {handleDiastolic}
-
         />
         <TextField
           required
           id="outlined-number"
           label="Systolic"
-
           onChange= {handleSystolic}
         />
         <Button
@@ -96,9 +109,12 @@ function AddMeasurement  ()  {
           >Submit
         </Button>
 
-        {errDiastolic ? <div>Error distolic</div> : <div>{diastolic}</div> }
-        {errSystolic ? <div>Error systolic</div> : <div>{systolic}</div> }
+        {errDiastolic ? <div>Error distolic</div> : <div>{diastolic}</div>}
+        {errSystolic ? <div>Error systolic</div> : <div>{systolic}</div>}
       </Stack>
+
+      <div>Medications Prescribed</div>
+
       <Stack>
         {recMedList.map((item, index) => {
           return <Button variant= 'outlined'
@@ -109,13 +125,15 @@ function AddMeasurement  ()  {
                   space={2}
                   key= {index}
                   value= {index}
-                  onClick= {handleMed}>
+                  onClick= {addMed}>
                   {item.name} {item.dosage}
                 </Button>
         })}
-
       </Stack>
+
       <div>-------------------------------------</div>
+      <div>Medications taken</div>
+
       <Stack>
         {takenMedList.map((item, index) => {
           return <Button variant= 'outlined'
@@ -126,40 +144,13 @@ function AddMeasurement  ()  {
                   space={2}
                   key= {index}
                   value= {index}
-                  onClick= {handleMed}>
+                  onClick= {removeMed}>
                   {item.name} {item.dosage}
                 </Button>
         })}
       </Stack>
-
     </div>
-
-
-
-
   )
 }
 
 export default AddMeasurement
-
-// <div>
-// {recMedList.map((item, index) => {
-//   return <button
-//           key= {index}
-//           value= {index}
-//           onClick= {handleMed}>
-//           {item.name} {item.dosage}
-//         </button>
-// })}
-
-// </div>
-
-// <div>
-// {takenMedList.map((item, index) => {
-//   return <button
-//           key= {index}
-//           value= {index}>
-//           {item.name} {item.dosage}
-//         </button>
-// })}
-// </div>
