f27bc7f4c2a5badca49402e7b32d007004b5549b
Brandon Hsu <brandon.j.hsu@gmail.com>
2023-01-05 16:04:05 -0800

    changed pagination style under History tab to include arrows in the pagination buttons

--- client/src/components/History.jsx
+++ client/src/components/History.jsx
@@ -2,17 +2,17 @@ import React, { useState, useEffect } from 'react';
 import Measurements from './Measurements.jsx';
 import Pagination from './Pagination.jsx';
 
+import ReactPaginate from 'react-paginate';
+
 import axios from 'axios';
 
 function History ({ userDataReversed })  {
 
   const [allMeasurements, setAllMeasurements] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [postsPerPage, setPostsPerPage] = useState(10);
+  const [pageNumber, setPageNumber] = useState(0);
+  const [postsPerPage, setPostsPerPage] = useState(5);
 
   useEffect(() => {
-    setLoading(true);
     axios.get('/getAllMeasurementsReversed')
       .then((results) => {
         setAllMeasurements(results.data);
@@ -20,22 +20,63 @@ function History ({ userDataReversed })  {
       .catch((err) => {
         console.log(err);
       })
-    setLoading(false);
   }, [])
-  const indexOfLastPost = currentPage * postsPerPage;
+
+  const indexOfLastPost = pageNumber * postsPerPage;
   const indexOfFirstPost = indexOfLastPost - postsPerPage;
-  const currentPosts = allMeasurements.slice(indexOfFirstPost, indexOfLastPost)
 
-  const paginate = (number) => setCurrentPage(number);
+  const currentPosts = allMeasurements
+    .slice(indexOfLastPost, indexOfLastPost + 10)
+    .map((measurement, index) => {
+      return (
+        <div key = {index} className= 'history-measurement-card'>
 
+          <div className= 'date-time-container'>
+            {new Date (measurement.created_at).toLocaleString()}
+          </div>
+          <div className= 'blood-pressure-container'>
+            {measurement.systolic}/{measurement.diastolic}
+          </div>
+          {measurement.meds_taken.length ?
+            <div className= 'medication-container'>
+              {measurement.meds_taken.map((taken, index) =>{
+                return (
+                  <div style={{marginRight: 10}} key= {index}>{taken}</div>
+                )
+              })}
+            </div> :
+            <div className= 'medication-container'>
+              No Medications Taken
+            </div>
+          }
+        </div>
+      )
+    })
+
+  const pageCount = Math.ceil(allMeasurements.length / postsPerPage);
+
+  const changePage = ({ selected }) => {
+    setPageNumber(selected);
+  }
   return (
     <div>
-      {userDataReversed.length > 0 ?
+
+      {allMeasurements.length > 0 ?
         <div className= 'history-container'>
-          <Measurements currentPosts= {currentPosts} loading= {loading}/>
-          <Pagination postsPerPage= {postsPerPage} totalPosts= {allMeasurements.length} paginate= {paginate}/>
+          {currentPosts}
+          <ReactPaginate
+            previousLabel ='<'
+            nextLabel= '>'
+            pageCount= {pageCount}
+            onPageChange= {changePage}
+            containerClassName= {'paginationButtons'}
+            nextLinkClassName= {'nextButton'}
+            previousLinkClassName= {'previousButton'}
+            disabledClassName= {'disablePaginated'}
+            activeClassName= {'activePaginated'}
+          />
         </div>
-        : null}
+        : <div> no data</div>}
 
     </div>
 
